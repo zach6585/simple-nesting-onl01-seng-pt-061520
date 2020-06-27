a862afcd2b55a95f908c8c379fa02f606d0043fa
zachary mines <zmines@wesleyan.edu>
2020-06-27 23:01:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,5 +130,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  # programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
