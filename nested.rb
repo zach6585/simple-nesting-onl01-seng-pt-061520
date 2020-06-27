7f948a9728fde9d0d1684889858ab2a3d84acac7
zachary mines <zmines@wesleyan.edu>
2020-06-27 22:59:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,10 +106,11 @@ def changing_alan
           :languages => ["C"]
         }
      }
+     
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = "GUI"
-     binding.pry
+     
      
 end
 
